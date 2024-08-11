ebee189d528db36cf8e78c527201efd9c9f1aea2
Andrey Arama <anarama@c3r2p5.42vienna.com>
2024-08-11 21:15:43 +0200

    create initial set up + screen. working on ship class

---
+++ alien_invasion.py
@@ -0,0 +1,25 @@
+import sys
+import pygame
+
+class ALienInvasion:
+
+	def __init__(self):
+		
+		pygame.init()
+		
+		self.screen = pygame.display.set_mode((1200, 800))
+		pygame.display.set_caption("Alien Invasion")
+
+		self.bg_color = (230, 230, 230)
+
+	def run_game(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					sys.exit()
+			self.screen.fill(self.bg_color)
+			pygame.display.flip()
+
+if __name__ == '__main__':
+	ai = ALienInvasion()
+	ai.run_game()
